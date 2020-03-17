b77c50e931c4e1752dd5aeb9677099f5b59f54f2
LFormery <formery.louise@gmail.com>
2020-03-16 19:50:55 -0700

    - Create a pug template - Install pug - Create a directory views - Create a file index.pug - Use the pug template

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const dotenv = require('dotenv').config();
 
 app.get('/', function(req, res) {
-    res.send('Hello moi');
+    res.render('index');
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
+
+app.set('view engine', 'pug');
