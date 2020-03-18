abab2ba24c764d7fd43f11fcf86a998087b41df0
LFormery <formery.louise@gmail.com>
2020-03-17 19:34:58 -0700

    08 - Pass variables to templates - Pass and use a variable for title

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const dotenv = require('dotenv').config();
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', { title: 'Qui prend quoi?' });
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
