4fde329f93f42a3114adeee0abe87e192d13660c
LFormery <formery.louise@gmail.com>
2020-03-17 20:13:55 -0700

    12 - Post to API - Install axios - Import et use axios

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.get('/', function(req, res) {
     res.render('index', { title: 'Qui prend quoi?' });
 });
 
 app.post('/party', function(req, res) {
+    axios
+        .post(`${process.env.API_URL}/party`, req.body)
+        .then(({data}) => console.log(data))
+        .catch((err) => console.error(err));
     //console.log(req.body);
     res.send('Post ok !');
 });
