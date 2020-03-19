de5d42b7cd6b9b9ab9c2fe9c52a3c1e3aa8e7ec3
LFormery <formery.louise@gmail.com>
2020-03-18 17:58:25 -0700

    20 - Link CSS & HTML

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
 app.get('/', function(req, res) {
