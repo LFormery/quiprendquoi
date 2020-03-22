7d6b9e51d2d35169f3320d159871e26f8db3e968
LFormery <formery.louise@gmail.com>
2020-03-22 04:29:01 +0100

    30 - Service Worker - Add sw.js file - Modify script - Few SCSS modifications

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -2,7 +2,7 @@ if (navigator.clipboard) {
     //console.log("Support du presse papier")
     document.querySelectorAll('[data-clipboard]').forEach(($clipboardEl) => {
         const $button = document.createElement('button');
-        $button.className = "fa fa-clipboard";
+        $button.className = "fa fa-clipboard full";
         $clipboardEl.parentNode.append($button);
         $button.addEventListener(
             'click',
---
+++ sw.js
@@ -0,0 +1,3 @@
+const name = 'bcalou';
+console.log(`Hello ${name}`);
+console.trace();
